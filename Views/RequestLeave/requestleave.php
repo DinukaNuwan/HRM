40e2dcff20cc9ad9ed149dba9f6b570d39fc5f79
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-06-14 13:45:27 +0530

    Leave request modified

--- Views/RequestLeave/requestleave.php
+++ Views/RequestLeave/requestleave.php
@@ -7,6 +7,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- fontawesome kit -->
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+
+    <!-- title logo -->
+    <link rel="shortcut icon" href="../../Resources/Images/hrms_logo.png">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -50,30 +57,86 @@
             height: 100px;
             object-fit: cover;
         }
+
+        .hr_logo img {
+            width: 100%;
+            height: 150px;
+            object-fit: cover;
+        }
+
+        .button {
+            width: fit-content;
+            border: none;
+            padding: 10px;
+            border-radius: 20px;
+            border: 1px solid gray;
+        }
+
+        .button:hover {
+            color: white;
+            background-image: url(../../Resources/Images/bg.jpg);
+            background-size: cover;
+        }
+
+        /* Navbar styles */
+
+        .sidenav {
+            height: 100%;
+            width: 250px;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            background-color: #ffffff;
+            overflow-x: hidden;
+            padding-top: 20px;
+        }
+
+        .sidenav a {
+            /* padding: 6px 8px 6px 16px; */
+            text-decoration: none;
+            /* color: #818181; */
+            display: block;
+            text-align: center;
+            margin: 20px;
+            padding: 10px;
+            border-bottom: 1px solid gray;
+        }
+
+        .sidenav a:hover {
+            color: white;
+            background-image: url(../../Resources/Images/bg.jpg);
+            background-size: cover;
+        }
+
+        @media screen and (max-height: 450px) {
+            .sidenav {
+                padding-top: 15px;
+            }
+
+            .sidenav a {
+                font-size: 18px;
+            }
+        }
+
+        .fas {
+            margin-right: 10px;
+        }
     </style>
 
 </head>
 
 <body>
 
-    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="../../Resources/Images/bg.jpg" alt="" width="30" height="24">
-                HRMS
-            </a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../Login/login.php" style="width: fit-content; background-color: white;border-radius: 20px;margin: 5px;">Logout</a>
-                    </li><li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../Login/login.php" style="width: fit-content; background-color: white;border-radius: 20px;margin: 5px;">Home</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="sidenav">
+        <div class="logo hr_logo">
+            <img src="../../Resources/Images/hrms_logo.png" alt="logo">
         </div>
-    </nav> -->
-
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+    </div>
     <div class="container">
         <div class="wrapper">
             <div class="logo">
@@ -84,11 +147,6 @@
             </div>
 
             <form>
-
-                <!-- <div class="form-outline mb-4">
-                    <label class="form-label" for="email">Employee ID</label>
-                    <input type="text" id="employee_id" class="form-control" placeholder="Employee ID" />
-                </div> -->
 
                 <div class="row">
                     <div class="col-md-6 mb-4">
@@ -108,6 +166,16 @@
                     </div>
                 </div>
 
+                <div class="row">
+                    <div class="form-outline col-md-6 mb-4">
+                        <label class="form-label" for="email">Leave Date</label>
+                        <input type="date" id="leave_date" class="form-control" placeholder="" />
+                    </div>
+                    <div class="form-outline col-md-6 mb-4">
+                        <label class="form-label" for="email">Return Date</label>
+                        <input type="date" id="return_date" class="form-control" placeholder="" />
+                    </div>
+                </div>
                 <div class="form-outline mb-4">
                     <label class="form-label" for="email">Reason</label>
                     <textarea class="form-control" placeholder="Enter a reason for the leave" id="reason"></textarea>
@@ -119,7 +187,7 @@
                 </div> -->
 
                 <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-primary" type="button" style="min-width: 100%;">Request</button>
+                    <button class=" button" type="button">Request</button>
                 </div>
             </form>
         </div>
