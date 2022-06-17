f7e39650260d27904add1bb22498c6facd7cb92b
Dinuka <dinukaw.19@cse.mrt.ac.lk>
2022-06-17 02:23:52 -0700

    init

--- Views/Requests/requests.php
+++ Views/Requests/requests.php
@@ -135,23 +135,24 @@
                     </div>
                 </form>
             </div>
-            <form>
+            <div row>
+                <form>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="email">First Name</label>
+                        <input type="text" id="firstname" class="form-control" placeholder="First Name" />
+                    </div>
+                    <hr>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="email">Last Name</label>
+                        <input type="text" id="lastname" class="form-control" placeholder="Last Name" />
+                    </div>
 
-                <div class="form-outline mb-4">
-                    <label class="form-label" for="email">First Name</label>
-                    <input type="text" id="firstname" class="form-control" placeholder="First Name" />
-                </div>
-                <hr>
-                <div class="form-outline mb-4">
-                    <label class="form-label" for="email">Last Name</label>
-                    <input type="text" id="lastname" class="form-control" placeholder="Last Name" />
-                </div>
 
-
-                <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-primary" type="button">Add User</button>
-                </div>
-            </form>
+                    <div class="text-center pt-1 mb-5 pb-1">
+                        <button class="btn btn-primary" type="button">Add User</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
