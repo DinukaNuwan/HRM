a410e618d89ed41af252d903bedd9b358556707a
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-06-19 23:14:17 +0530

    Add Employee Controller

--- Views/AddEmployee/addEmployee.php
+++ Views/AddEmployee/addEmployee.php
@@ -7,31 +7,31 @@
             <h4 class="mt-1 mb-4 pb-1">Add New Employee</h4>
         </div>
 
-        <form action="#" method="POST">
+        <form method="POST" action="<?= WEBROOT . 'addEmployee' ?>">
 
             <div class="personal">
                 <div class="row">
-                    <h6>Personal Details:</h6>
+                    <p><i class="fas fa-user-shield"></i>Personal Details:</p>
                 </div>
                 <div class="row">
                     <div class="form-outline col-md-6 mb-4">
                         <label class="form-label" for="first_name">First Name</label>
-                        <input type="text" id="first_name" name="first_name" class="form-control" placeholder="First Name" />
+                        <input type="text" id="first_name" name="first_name" class="form-control" placeholder="John" />
                     </div>
                     <div class="form-outline col-md-6 mb-4">
                         <label class="form-label" for="last_name">Last Name</label>
-                        <input type="text" id="last_name" name="last_name" class="form-control" placeholder="Last Name" />
+                        <input type="text" id="last_name" name="last_name" class="form-control" placeholder="Doe" />
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="form-outline col-md-6 mb-4">
                         <label class="form-label" for="email">Email</label>
-                        <input type="email" id="email" name="email" class="form-control" placeholder="Email" />
+                        <input type="email" id="email" name="email" class="form-control" placeholder="johndoe@email.com" />
                     </div>
                     <div class="form-outline col-md-6 mb-4">
                         <label class="form-label" for="mobile_no">Mobile Number</label>
-                        <input type="text" id="moble_no" name="mobile_no" class="form-control" placeholder="Mobile Number" />
+                        <input type="text" id="moble_no" name="mobile_no" class="form-control" placeholder="+947XXXXXXXXX" />
                     </div>
                 </div>
 
@@ -58,12 +58,12 @@
 
             <div class="employment">
                 <div class="row">
-                    <h6>Employment Deatis:</h6>
+                    <p><i class="fas fa-briefcase"></i>Employment Deatis:</p>
                 </div>
                 <div class="row">
                     <div class="form-outline col-md-6 mb-4">
                         <label class="form-label" for="firstname">Job Title</label>
-                        <select class="form-select" aria-label="Default select example">
+                        <select class="form-select" name="job_title" aria-label="Default select example">
                             <option selected>Select job title...</option>
                             <option value="HR Manager">HR Manager</option>
                             <option value="Accountant">Accountant</option>
@@ -82,7 +82,7 @@
                     </div>
                     <div class="form-outline col-md-6 mb-4">
                         <label class="form-label" for="pay_grade">Employment Status</label>
-                        <select class="form-select" id="pay_grade" name="pay_grade" aria-label="Default select example">
+                        <select class="form-select" id="emp_status" name="emp_status" aria-label="Default select example">
                             <option selected>Select employment status...</option>
                             <option value="Intern-Fulltime">Intern-Fulltime</option>
                             <option value="Intern-Parttime">Intern-Parttime</option>
@@ -97,28 +97,28 @@
 
             <div class="emergency">
                 <div class="row">
-                    <h6>Emergency Details:</h6>
+                    <p><i class="fas fa-exclamation-triangle"></i>Emergency Details:</p>
                 </div>
                 <div class="row">
                     <div class="form-outline mb-4">
-                        <label class="form-label" for="name">Name</label>
-                        <input type="text" id="emergency_name" name="emergency_name" class="form-control" placeholder="Name" />
+                        <label class="form-label" for="name">Name of the Contact Person</label>
+                        <input type="text" id="emergency_name" name="emergency_name" class="form-control" placeholder="John Doe" />
                     </div>
                 </div>
                 <div class="row">
                     <div class="form-outline col-md-6 mb-4">
-                        <label class="form-label" for="relationship">Relationship</label>
-                        <input type="text" id="relationship" name="relationship" class="form-control" placeholder="Ex: Father" />
+                        <label class="form-label" for="relationship">Relationship With Him/her</label>
+                        <input type="text" id="relationship" name="relationship" class="form-control" placeholder="Ex: Mother" />
                     </div>
                     <div class="form-outline col-md-6 mb-4">
                         <label class="form-label" for="mobile_no">Mobile Number</label>
-                        <input type="text" id="emergency_moble_no" name="emergency_mobile_no" class="form-control" placeholder="Mobile Number" />
+                        <input type="text" id="emergency_moble_no" name="emergency_mobile_no" class="form-control" placeholder="+947XXXXXXXXX" />
                     </div>
                 </div>
             </div>
 
             <div class="text-center pt-1 mb-5 pb-1">
-                <button class="btn btn-primary" name="submit" type="button">Add Employee</button>
+                <button class="btn btn-primary" name="submit" type="submit">Add Employee</button>
             </div>
         </form>
     </div>
