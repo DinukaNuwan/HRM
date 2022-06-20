ff81c7c6b5aebe703de3b45d7f312727fcce504a
Jalitha99 <jalithakalsara2@gmail.com>
2022-06-20 20:43:51 +0530

    login validation

--- Controllers/loginController.php
+++ Controllers/loginController.php
@@ -2,22 +2,42 @@
 
 class loginController extends Controller
 {
+    private $username;
+    private $password;
+    private $errors = [];
     function login()
     {
         require(ROOT . "Models/Login.php");
-
+        $data = $_POST;
         $model = new loginModel();
+        $this->secure_form($data);
 
-        if (isset($_POST['submit'])) {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            $msg = $model->authenticateUser($username, $password);
+        if (isset($data['submit'])) {
+            $this->username = $data['username'];
+            $this->password = $data['password'];
+
+            if ($this->checkEmptyFields()){
+                $this->errors [] = "empty_fields";
+            }
+            if ($this->checkFieldLength()){
+                $this->errors [] = "field_length_exceeded";
+            }
+
+            $msg = $model->authenticateUser($this->username, $this->password);
             if ($msg == 1) {
                 header('Location: allUsers');
             } else {
-                $this->set(array("error" => $msg));
+                $this->errors [] = $msg;
+                $this->set(array("error" => $this->errors));
+//                $this->set(array("error" => $msg));
             }
         }
-        $this->render("Login");
+    }
+    private function checkEmptyFields(){
+        return empty($this->username) or empty($this->password);
+    }
+    private function checkFieldLength(){
+        $field_lengths = array('username' => 45, 'password' => 255);
+        return (strlen($this->username) > 45 || strlen($this->password) > 255);
     }
 }
