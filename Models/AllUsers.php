0171d9d8609afa1c4f9c86893dc8e3fae09379be
akila99g <indurangakga.19@uom.lk>
2022-06-22 11:52:28 +0530

    allUsers Model : users count added

--- Models/AllUsers.php
+++ Models/AllUsers.php
@@ -7,14 +7,21 @@
             $numOfUsersPerPage=4;
             $to = $from + $numOfUsersPerPage;
 
+            //get num of users
+            $sql = "SELECT COUNT(emp_id) as count FROM `user`";
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute(array());
+
+            $numOfUsers = $statement->fetch(PDO::FETCH_ASSOC)['count'];
+
             //users personal data
             $sql = "SELECT  emp_id, firstname, lastname
-                FROM    ( SELECT    ROW_NUMBER() OVER ( ORDER BY emp_id ) AS RowNum, emp_id, firstname, lastname
-                        FROM      employee
-                        ) AS RowConstrainedResult
-                WHERE   RowNum >= :frm
-                    AND RowNum < :to
-                ORDER BY RowNum";
+            FROM    ( SELECT    ROW_NUMBER() OVER ( ORDER BY emp_id ) AS RowNum, emp_id, firstname, lastname
+                    FROM      employee JOIN user USING(emp_id)
+                    ) AS RowConstrainedResult
+            WHERE   RowNum >= :frm
+                AND RowNum < :to
+            ORDER BY RowNum";
             
             $statement = $this->pdo->prepare($sql);
             $statement->execute(array(
@@ -24,6 +31,7 @@
 
             $userData = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+            $userData['count'] = $numOfUsers; //get 'count' variable from the array to decide the nmr of pages 
             var_dump($userData);
 
             // return $userData;
