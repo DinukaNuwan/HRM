9bcb27f0ae84f40509670eafba86d3d1ab90999f
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-06-26 19:36:09 +0530

    Authorization for some pages

--- Controllers/addUserController.php
+++ Controllers/addUserController.php
@@ -14,70 +14,79 @@ class addUserController extends Controller
         $this->set(array('username' => $user->getUsername()));
         $this->set(array('role' => $user->getRole()));
 
-        require(ROOT . "Models/AddUser.php");
+        // Authorization based on user role
+        if ($user->getRole() == '4') {
+            header('Location: unauthorized');
+        } else {
 
-        $model = new addUserModel();
-        $data = $_POST;
-        if (isset($data['submit'])) {
-            $msg = "";
-            $this->emp_id = $data['employee_id'];
-            $this->username = $data['username'];
-            $this->password = $data['password'];
-//            $this->password = password_hash($data['password'], PASSWORD_DEFAULT);
-            $this->role = $data['role'];
-            $this->photo = $data['photo'];
-            $this->c_password = $data['c_password'];
+            require(ROOT . "Models/AddUser.php");
 
-            if ($this->checkFieldLengths()){
-                $this->errors [] = "empty fields";
-            }
-            if ($this->checkValidUsername()){
-                $this->errors [] = "enter a valid username";
-            }
-            if ($this->checkValidPassword()){
-                $this->errors [] = "Password must be at least 8 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character.";
-            }
-            if ($this->comparePasswords()){
-                $this->errors [] = "passwords doesn't match";
-            }
-            $this->password = password_hash($data['password'], PASSWORD_DEFAULT);
+            $model = new addUserModel();
+            $data = $_POST;
+            if (isset($data['submit'])) {
+                $msg = "";
+                $this->emp_id = $data['employee_id'];
+                $this->username = $data['username'];
+                $this->password = $data['password'];
+                //            $this->password = password_hash($data['password'], PASSWORD_DEFAULT);
+                $this->role = $data['role'];
+                $this->photo = $data['photo'];
+                $this->c_password = $data['c_password'];
 
-            if ($this->errors){
-                $this->set(array("error" => $this->errors));
-            }else{
-                $msg = $model->addNewUser($this->emp_id, $this->username, $this->password, $this->role, $this->photo);
-                if ($msg != 1){
-                    $this->errors [] = $msg;
+                if ($this->checkFieldLengths()) {
+                    $this->errors[] = "empty fields";
+                }
+                if ($this->checkValidUsername()) {
+                    $this->errors[] = "enter a valid username";
+                }
+                if ($this->checkValidPassword()) {
+                    $this->errors[] = "Password must be at least 8 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character.";
+                }
+                if ($this->comparePasswords()) {
+                    $this->errors[] = "passwords doesn't match";
+                }
+                $this->password = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                if ($this->errors) {
                     $this->set(array("error" => $this->errors));
+                } else {
+                    $msg = $model->addNewUser($this->emp_id, $this->username, $this->password, $this->role, $this->photo);
+                    if ($msg != 1) {
+                        $this->errors[] = $msg;
+                        $this->set(array("error" => $this->errors));
+                    }
+                }
+                //            $msg = $model->checkExists("user", "username", $this->username);
+                var_dump($this->errors);
+                if ($msg == 1) {
+                    header('Location: dashboard');
                 }
             }
-//            $msg = $model->checkExists("user", "username", $this->username);
-            var_dump($this->errors);
-            if ($msg == 1) {
-                header('Location: dashboard');
-            }
+            $this->render("AddUser");
         }
-        $this->render("AddUser");
     }
     // Check for field lengths
-    public function checkFieldLengths() {
-        $field_lengths = array('username' => 45, 'password' => 255, 'role' => 11 , 'emp_id' => 11);
+    public function checkFieldLengths()
+    {
+        $field_lengths = array('username' => 45, 'password' => 255, 'role' => 11, 'emp_id' => 11);
         return (strlen($this->username) > 45 || strlen($this->password) > 255);
     }
 
     // Valid username check
-    public function checkValidUsername() {
+    public function checkValidUsername()
+    {
         return !preg_match("/^[a-zA-Z]*$/", $this->username);
     }
 
     //valid password
-    public function checkValidPassword(){
+    public function checkValidPassword()
+    {
         $number = preg_match('@[0-9]@', $this->password);
         $uppercase = preg_match('@[A-Z]@', $this->password);
         $lowercase = preg_match('@[a-z]@', $this->password);
         $specialChars = preg_match('@[^\w]@', $this->password);
 
-        if(strlen($this->password) <  8|| !$number || !$uppercase || !$lowercase || !$specialChars){
+        if (strlen($this->password) <  8 || !$number || !$uppercase || !$lowercase || !$specialChars) {
             return true;
         }
         return false;
--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -32,7 +32,7 @@
                 <a href="adduser"><button class="btn btn-primary" name="submit" type="button">Add a new user</button></a>
             </div>
             <div class="text-center pt-1 mb-3 pb-1">
-                <a href=""><button class="btn btn-primary" name="submit" type="button">2</button></a>
+                <a href="addemployee"><button class="btn btn-primary" name="submit" type="button">Add a new employee</button></a>
             </div>
         <?php
         }
