7865a9f42088e2125b62530b3473a3d9c1384664
KasunIs <wgkasunisuranga@gmail.com>
2022-06-26 21:55:54 +0530

    Parameter passing

--- Controllers/loginController.php
+++ Controllers/loginController.php
@@ -1,7 +1,9 @@
 <?php
 
 class loginController extends Controller {
-    function login() {
+    function login($parameters) {
+
+        print_r($parameters);
 
         if (isset($_POST["login"])) {
             echo $_POST["email"];
--- Router.php
+++ Router.php
@@ -7,7 +7,7 @@ class Router
     {
         $url = trim($url);
 
-        if ($url == "/HRM/")
+        if ($url == "/edsa-HRM/")
         {
             $request->controller = "home";
             $request->action = "home";
@@ -18,15 +18,44 @@ class Router
             $explode_url = explode('/', $url);
             $explode_url = array_slice($explode_url, 2);
 
-            $request->controller = $explode_url[0];
+            //$request->controller = $explode_url[0];
             
+            //if (count($explode_url) == 1) {
+                //$request->action = $explode_url[0];
+                //$request->params = [];
+            //} else {
+            //$request->action = $explode_url[0];
+            //$request->params = [];
+            //$request->params = array_slice($explode_url, 1);
+
             if (count($explode_url) == 1) {
-                $request->action = $explode_url[0];
-                $request->params = [];
+                $request->controller = explode("?", end($explode_url))[0];
+                $request->action = explode("?", end($explode_url))[0];
+                $parameters = array_slice($explode_url, 1, count($explode_url) - 2);
             } else {
-                $request->action = $explode_url[1];
-                $request->params = array_slice($explode_url, 2);
+                $request->controller = $explode_url[0];
+                $request->action = $explode_url[0];
+                //$prm = array_slice($explode_url, 1, count($explode_url) - 2);
+                $parameters = array_merge(array(explode("?", end($explode_url))[0]), array_slice($explode_url, 1, count($explode_url) - 2));
             }
+
+            $associativeParameters = array();
+
+            //echo "a";
+
+            foreach (array_slice(explode("?", end($explode_url)), 1) as $element) {
+                $keyValue = explode("=", $element);
+                $associativeParameters[$keyValue[0]] = $keyValue[1];
+                //echo $element;
+            }
+
+            //foreach ($parameters as $parameter) {
+                //echo "e";
+                //cho $parameter;
+            //}
+
+            $request->params = [array($parameters, $associativeParameters)];
+            //}
         }
 
     }
