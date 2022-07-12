ea5311972c0a092531327f2cc637d9ddb1f09e42
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-07-12 21:39:20 +0530

    Leave request enhancement

--- Views/RequestLeave/requestleave.php
+++ Views/RequestLeave/requestleave.php
@@ -12,51 +12,64 @@ $dayAfterTomorrow = date('Y-m-d', strtotime("+2 days"));
             <h4 class="mt-1 mb-4 pb-1">Request Leave</h4>
         </div>
 
-        <form action="<?= WEBROOT ?>requestleave" method="POST" id="form" class="form" onsubmit="return checkInputs()">
+        <?php
+        if (isset($no_sup)) {
+        ?>
+            <p class="mb-3 text-center"><?= $no_sup ?></p>
+            <div class="nosup_img">
+                <img src="<?= WEBROOT ?>/Resources/Images/nosup.png" alt="logo" height="200">
+            </div>
+        <?php
+        } else {
+        ?>
+            <form action="<?= WEBROOT ?>requestleave" method="POST" id="form" class="form" onsubmit="return checkInputs()">
 
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-control form-outline form-input">
-                        <label class="form-label" for="leave_type">Leave Type</label>
-                        <select class="form-select" id="leave_type" name="leave_type" aria-label="Default select example">
-                            <option value="">Select leave type...</option>
-                            <option value="annual">Annual</option>
-                            <option value="casual">Casual</option>
-                            <option value="maternity">Maternity</option>
-                            <option value="no_pay">No-pay</option>
-                        </select>
-                        <small>Error message</small>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-control form-outline form-input">
+                            <label class="form-label" for="leave_type">Leave Type</label>
+                            <select class="form-select" id="leave_type" name="leave_type" aria-label="Default select example">
+                                <option value="">Select leave type...</option>
+                                <option value="annual">Annual</option>
+                                <option value="casual">Casual</option>
+                                <option value="maternity">Maternity</option>
+                                <option value="no_pay">No-pay</option>
+                            </select>
+                            <small>Error message</small>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-control form-outline form-input">
-                        <label class="form-label" for="leave_date">Leave Date</label>
-                        <input type="date" id="leave_date" name="leave_date" class="form-control" min=<?= strval($tomorrow) ?>>
-                        <small>Error message</small>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-control form-outline form-input">
+                            <label class="form-label" for="leave_date">Leave Date</label>
+                            <input type="date" id="leave_date" name="leave_date" class="form-control" min=<?= strval($tomorrow) ?>>
+                            <small>Error message</small>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-control form-outline form-input">
+                            <label class="form-label" for="return_date">Return Date</label>
+                            <input type="date" id="return_date" name="return_date" class="form-control" min=<?= strval($dayAfterTomorrow) ?>>
+                            <small>Error message</small>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="form-control form-outline form-input">
-                        <label class="form-label" for="return_date">Return Date</label>
-                        <input type="date" id="return_date" name="return_date" class="form-control" min=<?= strval($dayAfterTomorrow) ?>>
-                        <small>Error message</small>
-                    </div>
+
+                <div class="form-control form-outline form-input">
+                    <label class="form-label" for="reason">Reason</label>
+                    <textarea class="form-control" id="reason" name="reason" placeholder="Enter a reason for the leave"></textarea>
+                    <small>Error message</small>
                 </div>
-            </div>
 
-            <div class="form-control form-outline form-input">
-                <label class="form-label" for="reason">Reason</label>
-                <textarea class="form-control" id="reason" name="reason" placeholder="Enter a reason for the leave"></textarea>
-                <small>Error message</small>
-            </div>
-
-            <div class="text-center pt-1 mb-5 pb-1">
-                <button class="btn btn-primary" name="submit" type="submit">Request</button>
-            </div>
-        </form>
+                <div class="text-center pt-1 mb-5 pb-1">
+                    <button class="btn btn-primary" name="submit" type="submit">Request</button>
+                </div>
+            </form>
+        <?php
+        }
+        ?>
 
         <?php
         if (isset($error)) {
