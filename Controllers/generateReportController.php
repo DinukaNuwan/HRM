73698b7f7e7397889aea80e36965e81e3a8c7d9f
KasunIs <wgkasunisuranga@gmail.com>
2022-07-15 19:11:10 +0530

    Generate report correction

--- Controllers/generateReportController.php
+++ Controllers/generateReportController.php
@@ -6,24 +6,6 @@ class generateReportController extends Controller {
 
         $model = new generateReports();
 
-        //$model->getEmployeeRecords(array("job_title", "pay_grade"), array("A", "B"));
-
-        //print_r($_POST);
-        if (! empty($_POST['group'])) {
-            //print_r($_POST);
-            //$data = $model->getEmployeeDataGrouped($_POST['group']);
-            //print_r($data);
-            $this->set(["grouped" => $model->getEmployeeDataGrouped($_POST['group'])]);
-            //$model->getEmployeeDataGrouped("pay_grade");
-        } elseif (! empty($_POST['department'])) {
-            //print_r($_POST);
-            $model->getLeaveCount($_POST['from_date'], $_POST['to_date']);
-        }
-
-        $this->render("generatereport");
-    }
-}
-
         require(ROOT . "Classes/User.php");
         session_start();
         require_once("../Helpers/checkLogin.php");
@@ -44,7 +26,21 @@ class generateReportController extends Controller {
                 $_SESSION['department'] = $_POST['department'];
             }
         }
-        
-        $this->render("GenerateReport");
+
+        //$model->getEmployeeRecords(array("job_title", "pay_grade"), array("A", "B"));
+
+        //print_r($_POST);
+        if (! empty($_POST['group'])) {
+            //print_r($_POST);
+            //$data = $model->getEmployeeDataGrouped($_POST['group']);
+            //print_r($data);
+            $this->set(["grouped" => $model->getEmployeeDataGrouped($_POST['group'])]);
+            //$model->getEmployeeDataGrouped("pay_grade");
+        } elseif (! empty($_POST['department'])) {
+            //print_r($_POST);
+            $model->getLeaveCount($_POST['from_date'], $_POST['to_date']);
+        }
+
+        $this->render("generatereport");
     }
 }
