8d460764e71f63f934cc6e7dbe863640651f8b38
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-07-15 20:52:41 +0530

    Add supervisor improved

--- Views/AddSupervisor/addsupervisor.php
+++ Views/AddSupervisor/addsupervisor.php
@@ -33,24 +33,38 @@
                 <div class="col-md-6">
                     <div class="form-control form-outline form-input">
                         <label class="form-label" for="supervisor">Select Supervisor</label>
-                        <select class="form-select" aria-label="Default select example" name="supervisor" id="supervisor">
-                            <option value="">Select supervisor...</option>
+                        <?php
+                        if (count($supervisors) == 0) {
+                        ?>
+                            <p class="no_sup">No members currently available from this department</p>
+                        <?php
+                        } else {
+                        ?>
+                            <select class="form-select" aria-label="Default select example" name="supervisor" id="supervisor">
+                                <option value="">Select supervisor...</option>
+                                <?php
+                                foreach ($supervisors as $supervisor) {
+                                ?>
+                                    <option value="<?= $supervisor['emp_id'] ?>"><?= $supervisor['emp_id'] ?> - <?= $supervisor['firstname'] ?>&nbsp;<?= $supervisor['lastname'] ?></option>
                             <?php
-                            foreach ($supervisors as $supervisor) {
-                            ?>
-                                <option value="<?= $supervisor['emp_id'] ?>"><?= $supervisor['emp_id'] ?> - <?= $supervisor['firstname'] ?>&nbsp;<?= $supervisor['lastname'] ?></option>
-                            <?php
+                                }
                             }
                             ?>
-                        </select>
-                        <small>Error message</small>
+                            </select>
+                            <small>Error message</small>
                     </div>
                 </div>
             </div>
 
-            <div class="text-center pt-1 mb-5 pb-1">
-                <button class="btn btn-primary" type="submit" name="submit">Add Supervisor</button>
-            </div>
+            <?php
+            if (!count($supervisors) == 0) {
+            ?>
+                <div class="text-center pt-1 mb-5 pb-1">
+                    <button class="btn btn-primary" type="submit" name="submit">Add Supervisor</button>
+                </div>
+            <?php
+            }
+            ?>
         </form>
 
         <script src="<?= WEBROOT ?>Public/JavaScript/addSupervisorValidate.js" />
